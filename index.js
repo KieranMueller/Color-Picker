78b2eb72b35ee2746f98b71c9d1fc8c7e7c6e2a3
KieranMueller <121121198+KieranMueller@users.noreply.github.com>
2023-02-23 16:33:43 -0600

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const select = document.querySelector("#select")
 const selectAmount = document.querySelector("#select-amount")
 
 document.querySelector("#generate-btn").addEventListener("click", function() {
+    if(select.value.toLowerCase() == "select style") {
+        return alert("Please specify style")
+    }
+    if(selectAmount.value.toLowerCase() == "# of colors to return") {
+        return alert("Please specify amount of colors to be returned")
+    }
     container.innerHTML = ``
     let colorValue = colorPicker.value.slice(1,7)
     fetch(`https://www.thecolorapi.com/scheme?hex=${colorValue}&mode=${select.value.toLowerCase()}&count=${selectAmount.value}`)
